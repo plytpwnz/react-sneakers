136c90b8966a5dda991718f72cdb59d8bf6bf138
plytpwnz <oblomov.v.a@yandex.ru>
2025-04-28 16:30:29 +0300

    change input

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ export default function Home({
   return (
     <div className="content p-40">
       <div className="d-flex align-center justify-between mb-40">
-        <h1>{searchValue ? `Поиск по запросу: "${searchValue}"`: 'Все кроссовки'}</h1>
+        <h1>{searchValue ? `Поиск по запросу: ${searchValue}`: 'Все кроссовки'}</h1>
         <div className="search-block d-flex">
           <img src="/img/search.svg" alt="Search" />
           {searchValue && (
